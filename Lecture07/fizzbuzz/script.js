65247c6bfeb175cdad0edbde3f79a74352db46cf
Arnav Gupta <arnav@codingblocks.com>
2019-06-22 17:41:41 +0530

    fizzbuzz complete
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture07/fizzbuzz/script.js
+++ Lecture07/fizzbuzz/script.js
@@ -1,7 +1,30 @@
-let num;
 window.onload = function() {
-  console.log("document loaded")
- num = document.getElementById('num')
-}
+  const num = document.getElementById('num')
+  const print = document.getElementById('print')
+  const list = document.getElementById('list')
 
-console.log("script loaded")
+  print.onclick = function () {
+
+    const N = parseInt(num.value)
+
+    // let str = ""
+    // for (let i = 1; i <= N; i++) {
+    //   str += `<li> ${i} </li>`
+    // }
+    // list.innerHTML = str
+
+    const start = new Date().getTime()
+
+    for (let i = 1; i <= N; i++) {
+      let item = document.createElement('li')
+      item.textContent = i
+      list.appendChild(item)
+    }
+
+    console.log(new Date().getTime() - start)
+  }
+
+
+
+
+}
