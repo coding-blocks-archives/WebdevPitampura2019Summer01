633ef5f02145dbf62d9f3d76499ed3a39c9a2124
imdhruvgupta <gdhruv167@gmail.com>
2019-07-13 17:57:17 +0530

    chat app

--- Lecture16/sockets/server.js
+++ Lecture16/sockets/server.js
@@ -7,8 +7,24 @@ const server = http.createServer(app)
 
 const io = socket(server)
 
-io.on('connection', (xyz) => {
-    console.log("Connection Established ", xyz.id )
+io.on('connection', (socket) => {
+    console.log("Connection Established ", socket.id)
+
+    // socket.on('disconnect', () => {
+    //     console.log('Disconnected')
+    // })
+
+    socket.on("send_chat", (data) => {
+        console.log("Data Recieved ", data.message)
+
+        // socket.emit("recieve_chat", {
+        //     message: data.message
+        // })
+
+        socket.broadcast.emit("recieve_chat", {
+            message: data.message
+        })
+    })
 })
 
 app.use('/', express.static(__dirname + '/frontend'))
